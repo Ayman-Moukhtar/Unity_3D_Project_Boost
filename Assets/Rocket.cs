393efffe17f1c5ee29d51cf07255009edbbbc3fa
Ayman Moukhtar <ayman.moukhtar@outlook.com>
2019-04-24 19:04:14 +0200

    added rocket with basic movements

---
+++ Assets/Rocket.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    private Rigidbody _rocket;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rocket = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            _rocket.AddRelativeForce(Vector3.up);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            _rocket.transform.Rotate(Vector3.back);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            _rocket.transform.Rotate(Vector3.forward);
+        }
+    }
+}
